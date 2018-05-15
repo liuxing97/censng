b765442b1b48042c28176caab7dec6ec247c427a
liuxing <gex1217@censng.com>
2018-05-16 02:21:21 +0800

    msg post

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="/js/base64js.min.js"></script>
     <script type="text/javascript" src="/js/pages/common.js"></script>
     <link type="text/css" href="/css/font-awesome.min.css" rel="stylesheet" />
