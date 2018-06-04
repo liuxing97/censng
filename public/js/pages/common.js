800c1c82bce6dd8fe38b8b01de05658c0a9ba087
liuxing <2397514801@qq.com>
2018-06-04 14:27:46 +0800

    ------------1

--- public/js/pages/common.js
+++ public/js/pages/common.js
@@ -1,23 +1,24 @@
-window.onscroll = function(){
-    //顶部效果
-    var t = document.documentElement.scrollTop || document.body.scrollTop;
-    if(t > 66){
-        $(".header").css({"position":"fixed","top":"-75px","opacity":"0.98"});
-        $(".header-logo").css({"left":"-84","top":"88px","width":"45px","box-shawed":"0 0 10px #ccc"});
-        // $(".header-logo").hide();
-        $(".page").css({"padding-top":'138px'})
-    }else{
-        $(".header").css({"position":"relative","top":"0","opacity":"1"});
-        $(".header-logo").css({"left":"-84","top":"26px","width":"84px"});
-        $(".page").css({"padding-top":'0'});
-        // $(".header-logo").show();
-    }
-};
-
 wx.miniProgram.getEnv(function(res) {
-    // console.log(res.miniprogram) // true
-    $(".header").css({"position":"absolute","top":"-0","opacity":"0.98"});
+    console.log() // true
+    if(res.miniprogram){
+        $(".header").css({"position":"absolute","top":"0","opacity":"0.98"});
+    }else{
+        window.onscroll = function(){
+            //顶部效果
+            var t = document.documentElement.scrollTop || document.body.scrollTop;
+            if(t > 66){
+                $(".header").css({"position":"fixed","top":"-75px","opacity":"0.98"});
+                $(".header-logo").css({"left":"-84","top":"88px","width":"45px","box-shawed":"0 0 10px #ccc"});
+                // $(".header-logo").hide();
+                $(".page").css({"padding-top":'138px'})
+            }else{
+                $(".header").css({"position":"relative","top":"0","opacity":"1"});
+                $(".header-logo").css({"left":"-84","top":"26px","width":"84px"});
+                $(".page").css({"padding-top":'0'});
+                // $(".header-logo").show();
+            }
+        };
+    }
 });
-
 // left: -84px;
 // top: 26px;
